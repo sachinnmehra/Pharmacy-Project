7a5b0c02fdaec1ae4a15b8894815a2abe45576c1
sachinnmehra <79257710+sachinnmehra@users.noreply.github.com>
2021-03-24 11:40:36 +0530

    Update login.js

--- login.js
+++ login.js
@@ -4,16 +4,17 @@ let loginBtn = document.querySelector("#loginBtn");
 let link = document.querySelector("#anchol");
 if (window.localStorage) {
   console.log("heloooo");
-  localStorage.setItem("user", "edyoda");
-  localStorage.setItem("pass", "edyoda");
 
-  let user = localStorage.getItem("user", "edyoda");
-  let pass = localStorage.getItem("pass", "edyoda");
+  localStorage.setItem("user", userName.value);
+  localStorage.setItem("pass", password.value);
+
+  let user = localStorage.getItem("user", userName.value);
+  let pass = localStorage.getItem("pass", password.value);
   loginBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    if (user == userName.value && pass == password.value) {
+    if (user == pass) {
       alert("Login SuccessFull");
-      window.location.replace("../index.html");
+      window.location.replace("../order.html");
       console.log(window.location);
     } else {
       alert("wrong credentials");
